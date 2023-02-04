03bed9f3919e9a781a4870920b81e33807e23f4e
nebuloid28 <nebuloid28@gmail.com>
2023-02-05 00:06:47 +0900

    230205
    레이 캐스트 공부

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Cube : MonoBehaviour
+{
+    NavMeshAgent cube;
+    public static Cube instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, cube.destination) < 0.1f)
+        {
+            cube.isStopped = true;
+        }
+    }
+
+    public void Move(Vector3 dest)
+    {
+        cube.isStopped = false;
+        cube.destination = dest;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public RaycastHit hit;
+
     public static GameManager instance { get; private set; }
 
     private void Awake()
@@ -24,17 +26,52 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RayCast();
     }
 
     public void StartGame()
     {
 
+    }
+
+    public void RayCast()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Ball"))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    hit.collider.gameObject.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+                }
+                if (Input.GetMouseButtonDown(1))
+                {
+                    Rigidbody ball = hit.collider.gameObject.GetComponent<Rigidbody>();
+                    Vector3 dir = hit.transform.position - hit.point;
+                    ball.AddForce(dir * 5.0f, ForceMode.Impulse);
+                }
+            }
+
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Plane"))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    hit.collider.gameObject.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+                }
+                if (Input.GetMouseButtonDown(1))
+                {
+                    Cube.instance.Move(hit.point);
+                }
+            }
+        }
     }
 }
