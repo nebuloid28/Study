a739a6ee0a4945d9c0b422217738bca200432fa4
nebuloid28 <nebuloid28@gmail.com>
2023-02-09 20:50:03 +0900

    230209
    SerializeField, HideInInspector 공부

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /*
+    public int height;
+    public int weight;
+
+    [SerializeField]
+    private int height;
+    private int weight;
+    */
+
+    [HideInInspector]
+    public int height;
+    public int weight;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
